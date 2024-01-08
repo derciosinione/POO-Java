1c32d61af6cebcfca31dd33e4e6790d103a11cca
derciosinione <derciosinione@gmail.com>
2024-01-08 05:01:47 +0000

    Resolved case Orcamento

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Alunos aluno = new Alunos("Dercio");
-        aluno.Nome = "Cassia";
-        aluno.adicionarPrimeiraNota(12);
-        aluno.adicionarSegundaNota(14);
-        double mediaCalculada = aluno.calcularMedia();
-        System.out.println("A media do aluno "+ aluno.Nome +" eh "+ mediaCalculada);
+        Orcamento orcamento = new Orcamento();
+        InformarOrcamento(orcamento);
+        MostrarPrevisao(orcamento);
+    }
+
+    private static void InformarOrcamento(Orcamento orcamento) {
+        double valor;
+        System.out.printf(" 1: Semanal \n 2: Mensal \n Informe o tipo de orcamento desejado: ");
+        int ops = scanner.nextInt();
+
+        switch (ops){
+            case 1:
+                System.out.printf("Informe o valor que recebe semanalemente: ");
+                valor = scanner.nextDouble();
+                orcamento.setValorSemanal(valor);
+                break;
+            case 2:
+                System.out.printf("Informe o valor que recebe mensalmente: ");
+                valor = scanner.nextDouble();
+                orcamento.setValorMensal(valor);
+                break;
+            default:
+                System.out.println("Opcao invalida tente novamente!!!");
+        }
+    }
+
+    private static void MostrarPrevisao(Orcamento orcamento){
+        double valorDaPrevisao = 0;
+        int anos=0;
+
+        System.out.printf("Deseja uma previsao anual (y/n)? ");
+        String opcao = scanner.next();
+
+        if (opcao.toLowerCase().equals("y")){
+            System.out.printf("Informe quantos anos pretende para previsao: ");
+            anos = scanner.nextInt();
+            valorDaPrevisao = orcamento.valorDaPrevisaoAnual(anos);
+        }
+
+        printOrcamento(orcamento, valorDaPrevisao, anos);
+    }
+    public static void printOrcamento(Orcamento value, double valorDaPrevisao, int anos){
+        System.out.println("O valor que recebe durante a semana eh: "+ value.getValorSemanal());
+        System.out.println("O valor que recebe durante o mes eh: "+ value.getValorMensal());
+        System.out.println("O valor que recebe durante o ano eh: "+ value.getValorAnual());
+        System.out.println("O valor da previsao durante "+ anos +" eh: "+ valorDaPrevisao);
     }
 }
---
+++ src/Orcamento.java
@@ -0,0 +1,47 @@
+public class Orcamento {
+    private double ValorSemanal;
+    private double ValorMensal;
+    private double ValorAnual;
+
+    public double getValorSemanal() {
+        return ValorSemanal;
+    }
+
+    public void setValorSemanal(double valorSemanal) {
+        ValorSemanal = valorSemanal;
+        ValorMensal = valorSemanal * 4;
+        calcularValorAnual();
+    }
+
+    public double getValorMensal() {
+        return ValorMensal;
+    }
+
+    public void setValorMensal(double valorMensal) {
+        this.ValorMensal = valorMensal;
+        ValorSemanal = valorMensal / 4;
+        calcularValorAnual();
+    }
+
+    private void calcularValorAnual() {
+        ValorAnual = ValorMensal * 12;
+    }
+
+    public double getValorAnual() {
+        calcularValorAnual();
+        return ValorAnual;
+    }
+
+    public double valorDaPrevisaoAnual(int anos) {
+        return getValorAnual() * anos;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "ValorSemanal:" + ValorSemanal +
+                ", ValorMensal:" + ValorMensal +
+                ", ValorAnual:" + ValorAnual +
+                '}';
+    }
+}
